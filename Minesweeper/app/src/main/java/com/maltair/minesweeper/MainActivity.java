bd399a57d7b77d9ac12840a3325bb6632b2cf9ce
MiekuAlt <mc627781@dal.ca>
2017-10-18 01:26:22 -0300

    the game can now randomly generate mines and detect the neighbours on a grid

--- Minesweeper/app/src/main/java/com/maltair/minesweeper/MainActivity.java
+++ Minesweeper/app/src/main/java/com/maltair/minesweeper/MainActivity.java
@@ -10,11 +10,14 @@ import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    public final int NUM_MINES = 80;
+    public boolean gameStarted = false;
     public GridView gv;
     public String[] items = new String[81];
 
@@ -34,6 +37,15 @@ public class MainActivity extends AppCompatActivity {
         gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // TODO: For testing...
+                String debugMsg = "";
+                ArrayList buds = findBuddies(position);
+                for(int i = 0; i < buds.size(); i++){
+                    debugMsg += " " + buds.get(i);
+                }
+                debugDisp(debugMsg);
+
                 // Change the colour of the selected cell
                 LinearLayout ll = (LinearLayout) view;
                 TextView tv = ll.findViewById(R.id.textview);
@@ -48,8 +60,63 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 selectedCell = position;
+
+                if(!gameStarted) { // This activates on the first press
+                    gameStarted = true;
+                    spawnMines(position);
+                    CustomGridAdapter gridAdapter = new CustomGridAdapter(MainActivity.this, items);
+                    gv.setAdapter(gridAdapter);
+                }
             }
         });
+    }
+
+    // Runs the algorithm to populate mines in the gameworld everywhere except where the user pressed
+    public void spawnMines(int userSelection) {
+        // Initializing the array
+        for(int i = 0; i < items.length; i++) {
+            items[i] = "";
+        }
+
+        // Spawning the mines
+        int minesLeft = NUM_MINES;
+        while(minesLeft > 0) {
+            Random rand = new Random();
+            int newPos = rand.nextInt(items.length);
+            if(newPos != userSelection && !items[newPos].equals("*")) {
+                items[newPos] = "*";
+                minesLeft--;
+            }
+        }
+    }
+
+    // provides and array of the indexes of the grid's neighbours
+    public ArrayList findBuddies(int targetIndex) {
+        ArrayList buddies = new ArrayList();
+
+        // Checking which case the target is in (ex: top left corner)
+        // Corners
+        if(targetIndex == 0){
+            buddies.addAll(Arrays.asList(1,9,10));
+        } else if(targetIndex == 8){
+            buddies.addAll(Arrays.asList(7,16,17));
+        } else if(targetIndex == 72){
+            buddies.addAll(Arrays.asList(63,64,73));
+        } else if(targetIndex == 80){
+            buddies.addAll(Arrays.asList(70,71,79));
+        } else if(targetIndex < 9) { // top row
+            buddies.addAll(Arrays.asList(targetIndex - 1, targetIndex + 1, targetIndex + 8, targetIndex + 9, targetIndex + 10));
+        } else if(targetIndex > 71) { // bottom row
+            buddies.addAll(Arrays.asList(targetIndex - 1, targetIndex + 1, targetIndex - 8, targetIndex - 9, targetIndex - 10));
+        } else if(targetIndex % 9 == 0) { // first column
+            buddies.addAll(Arrays.asList(targetIndex + 1, targetIndex + 9, targetIndex + 10, targetIndex - 8, targetIndex - 9));
+        } else if(targetIndex % 9 == 8) { // last column
+            buddies.addAll(Arrays.asList(targetIndex - 1, targetIndex + 8, targetIndex + 9, targetIndex - 9, targetIndex - 10));
+        } else { // all the rest
+            buddies.addAll(Arrays.asList(targetIndex - 1, targetIndex + 1, targetIndex - 8, targetIndex + 8, targetIndex - 9, targetIndex + 9, targetIndex - 10, targetIndex + 10));
+        }
+
+        return buddies;
     }
 
     // Sets the value of the selected grid based on the value of the button pressed
@@ -78,17 +145,19 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        sortGrid();
+
 
         // Updating the grid's view
         CustomGridAdapter gridAdapter = new CustomGridAdapter(MainActivity.this, items);
         gv.setAdapter(gridAdapter);
     }
 
-    // Sorts the values of the grid alphabetically by row
-    private void sortGrid() {
-        for(int i = 0; i < 9; i++) {
-            Arrays.sort(items, i * 9, (i+1) * 9);
-        }
+
+    // This is just for debugging
+    // TODO: Remove this!
+    public void debugDisp(String msg) {
+        TextView debugView = (TextView) findViewById(R.id.debugView);
+        debugView.setText(msg);
     }
-}
+
+} // end of MainActivity class
